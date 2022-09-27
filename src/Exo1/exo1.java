b10b86523c7f925f3e478a9c9ac65a479a24cd45
Bahsiik <o.mist1303@gmail.com>
2022-09-27 13:29:19 +0200

    part2 exo1

--- src/Exo1/exo1.java
+++ src/Exo1/exo1.java
@@ -3,20 +3,30 @@ package Exo1;
 import utils.utils;
 
 public class exo1 {
-    public static void main(String[] args) {
-        class Archimede {
-            public double Cn(double x, double y) {
-                return  2 * x * y / (x + y);
-            }
-
-            public double Ln(double x, double y) {
-                return Math.sqrt(x * y);
-            }
-        }
-
+    public void main(String[] args) {
         Archimede archimede = new Archimede();
         System.out.println(archimede.Cn(utils.getDouble("X"), utils.getDouble("Y")));
         System.out.println(archimede.Ln(utils.getDouble("X"), utils.getDouble("Y")));
+        double[] array = An(5);
         utils.Continue(utils.getExoNumber());
+    }
+
+    class Archimede {
+        public static double Cn(double x, double y) {
+            return  2 * x * y / (x + y);
+        }
+
+        public static double Ln(double x, double y) {
+            return Math.sqrt(x * y);
+        }
+    }
+    private double[] An(int n) {
+        double x = 4;
+        double y = (2*Math.sqrt(2));
+        for (int i = 0; i < n; i++) {
+            x = Archimede.Cn(x, y);
+            y = Archimede.Ln(x, y);
+        }
+        return new double[]{x, y};
     }
 }
