f51c0a9c6ea91b6ca0c5e0d08ed87d7a8099758f
antaww <anta.pizzetta@gmail.com>
2022-09-27 13:09:53 +0200

    feat: exo1 first part

--- src/Exo1/exo1.java
+++ src/Exo1/exo1.java
@@ -4,7 +4,19 @@ import utils.utils;
 
 public class exo1 {
     public static void main(String[] args) {
+        class Archimede {
+            public double Cn(double x, double y) {
+                return  2 * x * y / (x + y);
+            }
 
+            public double Ln(double x, double y) {
+                return Math.sqrt(x * y);
+            }
+        }
+
+        Archimede archimede = new Archimede();
+        archimede.Cn(utils.getDouble("X"), utils.getDouble("Y"));
+        archimede.Ln(utils.getDouble("X"), utils.getDouble("Y"));
         utils.Continue(utils.getExoNumber());
     }
 }
