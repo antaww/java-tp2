bee3e95da24b786dc0c8b5744edba472ee2c1847
Bahsiik <o.mist1303@gmail.com>
2022-10-04 09:36:23 +0200

    fix bug menu

--- src/terminal/menu/menu.java
+++ src/terminal/menu/menu.java
@@ -23,19 +23,21 @@ public class menu {
         System.out.println("0. Quitter");
         System.out.println("G. GUI");
         String choix = utils.getString();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (choix.equals(String.valueOf(i+1))) {
-                try {
-                    Class.forName("terminal.Exo" + (i+1) + ".exo" + (i+1)).getMethod("main", String[].class).invoke(null, (Object) null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
         if (choix.equals("0")) {
             utils.SystemOut();
         } else if(choix.equals("G") || choix.equals("g")) {
             gui.gui.main(args);
+        } else if (choix.matches("[1-4]+")) {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (choix.equals(String.valueOf(i+1))) {
+                    try {
+                        Class.forName("terminal.Exo" + (i+1) + ".exo" + (i+1)).getMethod("main", String[].class).invoke(null, (Object) null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
         } else {
             System.out.println("Choix invalide");
             menu.main(args);
