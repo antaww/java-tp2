f67b313f954b54c32a7da553877edf945671108c
antaww <anta.pizzetta@gmail.com>
2022-10-04 12:28:59 +0200

    feat: exo2 changed

--- src/terminal/Exo2/exo2.java
+++ src/terminal/Exo2/exo2.java
@@ -14,7 +14,18 @@ public class exo2 {
     }
 
     public static double MesImpots(double gains){
-        double taux = (gains <= 10225) ? 0 : (gains <= 26070) ? 0.11 : (gains <= 74545) ? 0.3 : (gains <= 160336) ? 0.41 : 0.45;
-        return taux * gains;
+        double impots = 0;
+        if (gains < 10225){
+            impots = 0;
+        } else if (gains >= 10226 && gains <= 26070){
+            impots = 0.11 * (gains - 10225);
+        } else if (gains >= 26071 && gains <= 74545){
+            impots = 0.3 * (gains - 26070) + 0.11 * (26070 - 10225);
+        } else if (gains >= 74546 && gains <= 160336){
+            impots = 0.41 * (gains - 74545) + 0.3 * (74545 - 26070) + 0.11 * (26070 - 10225);
+        } else if (gains >= 160337){
+            impots = 0.45 * (gains - 160336) + 0.41 * (160336 - 74545) + 0.3 * (74545 - 26070) + 0.11 * (26070 - 10225);
+        }
+        return impots;
     }
 }
